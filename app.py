2a70c120b736b8e8bf6db45f0d10270ecda4bb85
nandhanrao <nandhanrao09@gmail.com>
2024-10-22 18:20:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,36 @@
 import os
 import warnings
-
 import gradio as gr
 import lightning as L
 from lightning.app.components.serve import ServeGradio
 from PIL import Image
-
 from src.llava_wrapper import LLaVA
 from src import prompt_handler
 
 warnings.filterwarnings("ignore")
 
-
 class LitGradio(ServeGradio):
-
     inputs = [
         gr.Image(type="filepath", label='Image'),
-        gr.Textbox(label="Hints", placeholder="Menu name, brocolli 3 pieces, steak 150g..."),
+        gr.Textbox(label="Hints", placeholder="Menu name, broccoli 3 pieces, steak 150g..."),
     ]
     outputs = [
-        gr.outputs.Textbox(label="Menu Name"),
-        gr.outputs.Textbox(label="Description"),
-        gr.outputs.JSON(label="Nutrients"),
+        gr.Textbox(label="Menu Name"),
+        gr.Textbox(label="Description"),
+        gr.JSON(label="Nutrients"),
     ]
-
     examples = [
-        [
-            os.path.join(os.path.dirname(__file__), "assets", "fruits.jpg"),
-            None,
-        ],
-        [
-            os.path.join(os.path.dirname(__file__), "assets", "pasta.jpeg"),
-            None,
-        ],
-        [
-            os.path.join(os.path.dirname(__file__), "assets", "pizza.jpeg"),
-            None,
-        ],
-        [
-            os.path.join(os.path.dirname(__file__), "assets", "salad.jpeg"),
-            None,
-        ],
-        [
-            os.path.join(os.path.dirname(__file__), "assets", "waffle.jpeg"),
-            None,
-        ],
+        [os.path.join(os.path.dirname(__file__), "assets", "fruits.jpg"), None],
+        [os.path.join(os.path.dirname(__file__), "assets", "pasta.jpeg"), None],
+        [os.path.join(os.path.dirname(__file__), "assets", "pizza.jpeg"), None],
+        [os.path.join(os.path.dirname(__file__), "assets", "salad.jpeg"), None],
+        [os.path.join(os.path.dirname(__file__), "assets", "waffle.jpeg"), None],
     ]
 
     def __init__(self):
         super().__init__()
         self.ready = False
+        self.model = self.build_model()  # Initialize model here
 
     def predict(self, image_path, hints):
         print("Start prediction")
@@ -63,7 +44,7 @@ class LitGradio(ServeGradio):
         description = dict_out.get('description', 'Not found')
         nutrients = dict_out.get('nutrients', 'Not found')
         nutrients_dict = {}
-        if type(nutrients) is list:
+        if isinstance(nutrients, list):
             for nutrient in nutrients:
                 name = nutrient['nutrientName']
                 value = nutrient['nutritionalValue']
@@ -71,13 +52,11 @@ class LitGradio(ServeGradio):
                 nutrients_dict[name] = f"{value} {unit}"
 
         print('Done')
-
         return menu_name, description, nutrients_dict
 
     def build_model(self):
         model = LLaVA()
         self.ready = True
         return model
-
 
 app = L.LightningApp(LitGradio())
